046fb893f28728c8cec01eb0d2309b73f57adc25
Farcem11 <farcem90@hotmail.com>
2016-05-29 21:46:22 -0600

    first commit

---
+++ app/src/main/java/com/android/geolocalization/MapsActivity.java
@@ -0,0 +1,127 @@
+package com.android.geolocalization;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    Context context;
+    Marker now;
+    double latitude;
+    double longitude;
+    LocationManager mLocationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        context = this;
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mapFragment.getMapAsync(this);
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+        {
+            buildAlertMessage("Your GPS is disabled, please enabled it");
+        }
+    }
+
+    private void buildAlertMessage(String pMessage) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(pMessage)
+                .setCancelable(false)
+                .setPositiveButton("Enable", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            return;
+        mMap.setMyLocationEnabled(true);
+
+        final LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location)
+            {
+                updateLocation(getLastKnownLocation());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+    }
+
+    private void updateLocation(Location location)
+    {
+        if (now != null)
+            now.remove();
+        longitude = location.getLongitude();
+        latitude = location.getLatitude();
+        LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        now = mMap.addMarker(new MarkerOptions().position(myLocation).title("Me"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation,20));
+    }
+
+    private Location getLastKnownLocation()
+    {
+        mLocationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        List<String> providers = mLocationManager.getProviders(true);
+        Location bestLocation = null;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            for (String provider : providers) {
+                Location l = mLocationManager.getLastKnownLocation(provider);
+                if (l == null) {
+                    continue;
+                }
+                if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                    bestLocation = l;
+                }
+            }
+            return bestLocation;
+        }
+        else
+            return null;
+    }
+}
