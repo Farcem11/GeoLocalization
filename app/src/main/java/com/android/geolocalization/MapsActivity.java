6bc19651da5c14d86859cf030cbf869e6f128ebe
Farcem11 <farcem90@hotmail.com>
2016-06-07 14:45:18 -0600

    Display plants info

--- app/src/main/java/com/android/geolocalization/MapsActivity.java
+++ app/src/main/java/com/android/geolocalization/MapsActivity.java
@@ -1,10 +1,12 @@
 package com.android.geolocalization;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.location.Location;
@@ -17,6 +19,12 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -25,10 +33,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -37,6 +41,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
@@ -49,7 +54,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     double longitude;
     LocationManager mLocationManager;
     ArrayList<Plant> plants;
-
+    private HashMap<Marker, Plant> markerMapPlant = new HashMap<>();
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -70,7 +75,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         builder.setMessage(pMessage)
                 .setCancelable(false)
                 .setPositiveButton("Enable", new DialogInterface.OnClickListener() {
-                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                    public void onClick(final DialogInterface dialog, final int id) {
                         startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
                     }
                 });
@@ -78,10 +83,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         alert.show();
     }
 
+    public void showPlant(Plant plant)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.plant_info, null);
+        ((TextView) dialogView.findViewById(R.id.textViewName)).setText(plant.get_Name());
+        ((TextView) dialogView.findViewById(R.id.textViewPlanter)).setText(plant.get_Planter());
+        ((TextView) dialogView.findViewById(R.id.textViewDonor)).setText(plant.get_Donor());
+        ((ImageView) dialogView.findViewById(R.id.imageViewPlant)).setImageBitmap(plant.get_Image());
+
+        builder.setView(dialogView)
+        .setPositiveButton("Cerrar", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int id)
+            {
+                dialog.cancel();
+            }
+        });
+
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener()
+        {
+            @Override
+            public boolean onMarkerClick(Marker marker)
+            {
+                showPlant(markerMapPlant.get(marker));
+                return false;
+            }
+        });
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
             return;
         mMap.setMyLocationEnabled(true);
@@ -109,23 +146,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         };
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 20, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 20, locationListener);
+/*
         Bitmap image = null;
         try
         {
-            image = new getBitmapFromURL().execute("http://i.ggimgs.net/categories/387.jpg").get();
+            image = new getBitmapFromURL().execute("http://cdn.playbuzz.com/cdn/35df18d9-8369-4566-991a-c7379c53e34d/62abd3fa-f460-4d80-8ff6-f52300526cbb.jpg").get();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
-        DataBase.addPlant(new Plant(0, "Planta 3", image, 9.9753042, -84.0184231, "Kevin", "Kevin"));
-
+        DataBase.addPlant(new Plant(0, "Planta 5", image, 9.9756239,-84.0180020, "Kevin", "Kevin"));
+*/
         plants = DataBase.getPlants();
+        markerMapPlant.clear();
         for (Plant plant : plants)
-            mMap.addMarker(new MarkerOptions().position(new LatLng(plant.get_Latitude(), plant.get_Longitude())));
+        {
+            Marker marker = mMap.addMarker(new MarkerOptions().position(new LatLng(plant.get_Latitude(), plant.get_Longitude())));
+            markerMapPlant.put(marker, plant);
+        }
     }
 
     private void updateLocation(Location location)
@@ -133,7 +174,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         longitude = location.getLongitude();
         latitude = location.getLatitude();
         LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation,15));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation,20));
     }
 
     private Location getLastKnownLocation()
